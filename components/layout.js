3bc5d1ca238b7f31db65421b7128edbc5bdbfa5a
Jared Engler <jared.lite@gmail.com>
2020-12-22 15:07:51 -0600

    add name and title based off name to homepage

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'jared.engler'
+export const siteTitle = `${name}.blog`
 
 export default function Layout({ children, home }) {
   return (
